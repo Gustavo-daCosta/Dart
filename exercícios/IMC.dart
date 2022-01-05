124c9dc68cd754eeb7b25549da41aab9e60b0104
Gustavo Costa <80484719+Gustavo-daCosta@users.noreply.github.com>
2022-01-05 11:25:46 -0300

    Update IMC.dart

--- exercícios/IMC.dart
+++ exercícios/IMC.dart
@@ -1,35 +1,31 @@
 import 'dart:io';
 
 main() {
-  print('====== Digite seu Peso ======');
-  var textPeso = stdin.readLineSync();
-  var peso = 1;
-  if (textPeso != null) {
-    peso = int.parse(textPeso);
-  }
+  print('======== IMC ========');
+  stdout.write('Peso: ');
+  String? inputPeso = stdin.readLineSync();
+  stdout.write('Altura: ');
+  String? inputAltura = stdin.readLineSync();
+  
+  if (inputPeso != null && inputAltura != null) {
+    int peso = int.parse(inputPeso);
+    double altura = double.parse(altura);
 
-  print('====== Digite sua altura ======');
-  var textAltura = stdin.readLineSync();
-  var altura = 1.1;
-  if (textAltura != null) {
-    altura = double.parse(textAltura);
-  }
+    double calcIMC = peso / (altura * altura);
+    print('Seu IMC é $calcIMC');
 
-  var calcIMC = peso / (altura * altura);
-
-  print('Seu IMC é $calcIMC');
-
-  if (calcIMC < 18.5) {
-    print('Abaixo do peso');
-  } else if (calcIMC >= 18.5 && calcIMC < 25) {
-    print('Peso Normal');
-  } else if (calcIMC >= 25 && calcIMC < 30) {
-    print('Sobrepeso');
-  } else if (calcIMC >= 30 && calcIMC < 35) {
-    print('Obesidade grau 1');
-  } else if (calcIMC >= 35 && calcIMC < 40) {
-    print('Obesidade grau 2');
-  } else {
-    print('Obesidade grau 3');
+    if (calcIMC < 18.5) {
+      print('Abaixo do peso');
+    } else if (calcIMC >= 18.5 && calcIMC < 25) {
+      print('Peso Normal');
+    } else if (calcIMC >= 25 && calcIMC < 30) {
+      print('Sobrepeso');
+    } else if (calcIMC >= 30 && calcIMC < 35) {
+      print('Obesidade grau 1');
+    } else if (calcIMC >= 35 && calcIMC < 40) {
+      print('Obesidade grau 2');
+    } else if (calcIMC >= 40) {
+      print('Obesidade grau 3');
+    }
   }
 }
